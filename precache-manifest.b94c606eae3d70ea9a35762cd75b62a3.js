504cc4110692701da44a48948c625dd23c5d7cce
king@Aj <anchit764@gmail.com>
2020-02-18 13:21:37 +0530

    Updates

---
+++ precache-manifest.b94c606eae3d70ea9a35762cd75b62a3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "af68a9e6ba2ea094ed68cb89ee709cc3",
+    "url": "/ContactManager/index.html"
+  },
+  {
+    "revision": "de2955608ec95d6c66f0",
+    "url": "/ContactManager/static/css/2.47e06e2e.chunk.css"
+  },
+  {
+    "revision": "07339195da521f8108de",
+    "url": "/ContactManager/static/css/main.36497136.chunk.css"
+  },
+  {
+    "revision": "de2955608ec95d6c66f0",
+    "url": "/ContactManager/static/js/2.d45c16c0.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/ContactManager/static/js/2.d45c16c0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "07339195da521f8108de",
+    "url": "/ContactManager/static/js/main.ae8714d1.chunk.js"
+  },
+  {
+    "revision": "b57d18ab4eddb72ef6ee",
+    "url": "/ContactManager/static/js/runtime-main.5ccda770.js"
+  }
+]);
